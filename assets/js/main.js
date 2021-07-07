82acfa4e65b3e05ec82e7bba0fdcb7e0dab46e22
Alyssa <alyssanterry468@gmail.com>
2021-07-07 00:07:48 -0400

    Adding content!
    Home, About, Resume, Contact pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -238,4 +238,18 @@
     });
   });
 
+/**PHP Form */
+// <?php
+// $email_address = $_POST['email_address'];
+// $feedback = $_POST['feedback'];
+
+function filter_email_header($form_field) {  
+  return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
+  }
+
+$email_address  = filter_email_header($email_address);
+
+$headers = "From: $email_addressn";
+$sent = mail('AlyssaNTerry468@gmail.com', 'Feedback Form Submission', $feedback, $headers);
+
 })()

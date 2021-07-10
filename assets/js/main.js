459b9f40b94e9e1168a2d7d9511d4994cafc7c07
Alyssa <alyssanterry468@gmail.com>
2021-07-09 22:59:13 -0400

    Added L5PCAC
    Added first case study and started others. Adjusted social icons in header. Added link to PDF resume.

--- assets/js/main.js
+++ assets/js/main.js
@@ -243,13 +243,13 @@
 // $email_address = $_POST['email_address'];
 // $feedback = $_POST['feedback'];
 
-function filter_email_header($form_field) {  
-  return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
-  }
+// function filter_email_header($form_field) {  
+//   return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
+//   }
 
-$email_address  = filter_email_header($email_address);
+// $email_address  = filter_email_header($email_address);
 
-$headers = "From: $email_addressn";
-$sent = mail('AlyssaNTerry468@gmail.com', 'Feedback Form Submission', $feedback, $headers);
+// $headers = "From: $email_addressn";
+// $sent = mail('AlyssaNTerry468@gmail.com', 'Feedback Form Submission', $feedback, $headers);
 
 })()
